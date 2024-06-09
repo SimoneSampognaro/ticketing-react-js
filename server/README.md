c0aa9faa84ea9a2a8aa90df241d02c548b2deaff
SimoneSampognaro <simosampo96@gmail.com>
2024-06-09 19:05:31 +0200

    title

---
+++ server/README.md
@@ -0,0 +1,17 @@
+# Ticketing System
+## The database has some users:
+
+## Summary of endpoints
+
+`/api`
+
+## Functionality of API
+- informations
+
+``` 
+json
+```
+
+
+
+
---
+++ server/dao.js
@@ -0,0 +1,12 @@
+"use strict";
+/* Data Access Object (DAO) */
+
+/* const sqlite = require('sqlite3');
+const dayjs = require('dayjs');
+
+// open the database
+const db = new sqlite.Database('qa.db', (err) => {
+  if(err) throw err;
+}); */
+
+// exports.function = () => {}
